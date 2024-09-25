76cea0f79868d53feeade1ef4e627484a4f5f123
GiWoonHwang <85157729+GiWoonHwang@users.noreply.github.com>
2024-09-25 10:08:54 +0900

    item7

--- src/effectivejava/chapter2/item7/StackPrc.java
+++ src/effectivejava/chapter2/item7/StackPrc.java
@@ -1,51 +1,38 @@
 package effectivejava.chapter2.item7;
-import java.util.*;
 
-// 코드 7-1 메모리 누수가 일어나는 위치는 어디인가? (36쪽)
-public class Stack {
+import java.util.Arrays;
+
+
+public class StackPrc {
     private Object[] elements;
+
     private int size = 0;
+
     private static final int DEFAULT_INITIAL_CAPACITY = 16;
 
-    public Stack() {
+    public StackPrc() {
         elements = new Object[DEFAULT_INITIAL_CAPACITY];
     }
 
-    public void push(Object e) {
+    public  void push(Object e) {
         ensureCapacity();
         elements[size++] = e;
     }
 
+    // 스택에서 요소를 제거할 때, size(저장된 요소의 개수)를 감소시키기만 하고 해당 배열의 인덱스를 null로 초기화 하지 않는다. 따라서 해당 위치에 요소가 남아있고 객체에 대한 참조가 유지된다.
+    // 참조가 유지되면 메모리에서 해제되지 않고 결과적으로 메모리 누수가 발생된다.
     public Object pop() {
         if (size == 0)
-            throw new EmptyStackException();
+                throw new EmptyStackException();
         return elements[--size];
     }
 
-    /**
-     * 원소를 위한 공간을 적어도 하나 이상 확보한다.
-     * 배열 크기를 늘려야 할 때마다 대략 두 배씩 늘린다.
-     */
+
     private void ensureCapacity() {
         if (elements.length == size)
             elements = Arrays.copyOf(elements, 2 * size + 1);
     }
 
-//    // 코드 7-2 제대로 구현한 pop 메서드 (37쪽)
-//    public Object pop() {
-//        if (size == 0)
-//            throw new EmptyStackException();
-//        Object result = elements[--size];
-//        elements[size] = null; // 다 쓴 참조 해제
-//        return result;
-//    }
 
-    public static void main(String[] args) {
-        Stack stack = new Stack();
-        for (String arg : args)
-            stack.push(arg);
 
-        while (true)
-            System.err.println(stack.pop());
-    }
 }
