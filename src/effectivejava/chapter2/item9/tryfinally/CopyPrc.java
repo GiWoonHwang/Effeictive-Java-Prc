d2fbfd4e30b09ef1980c17c1c74779ba2ceb6241
GiWoonHwang <85157729+GiWoonHwang@users.noreply.github.com>
2024-09-27 10:10:26 +0900

    item9

--- src/effectivejava/chapter2/item9/tryfinally/CopyPrc.java
+++ src/effectivejava/chapter2/item9/tryfinally/CopyPrc.java
@@ -2,28 +2,32 @@ package effectivejava.chapter2.item9.tryfinally;
 
 import java.io.*;
 
-public class Copy {
+/**
+ * 현재 코드의 문제점
+ * FileInputStream 예외가 발생한 후, OutputStream 예외가 발생하면 첫 번째 예외는 사라지고 두 번째 예외만 전달된다.
+ */
+public class CopyPrc {
+
     private static final int BUFFER_SIZE = 8 * 1024;
 
-    // 코드 9-2 자원이 둘 이상이면 try-finally 방식은 너무 지저분하다! (47쪽)
     static void copy(String src, String dst) throws IOException {
         InputStream in = new FileInputStream(src);
         try {
             OutputStream out = new FileOutputStream(dst);
-            try {
+            try{
                 byte[] buf = new byte[BUFFER_SIZE];
                 int n;
                 while ((n = in.read(buf)) >= 0)
-                    out.write(buf, 0, n);
+                    out.write(buf, 0,n);
             } finally {
                 out.close();
             }
         } finally {
             in.close();
         }
-    }
 
-    public static void main(String[] args) throws IOException {
+    }
+    public  static void main(String[] args) throws IOException {
         String src = args[0];
         String dst = args[1];
         copy(src, dst);
--- src/effectivejava/chapter2/item9/trywithresources/TopLineWithDefaultPrc.java
+++ src/effectivejava/chapter2/item9/trywithresources/TopLineWithDefaultPrc.java
@@ -5,16 +5,18 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class TopLineWithDefault {
-    // 코드 9-5 try-with-resources를 catch 절과 함께 쓰는 모습 (49쪽)
+public class TopLineWithDefaultPrc {
+
     static String firstLineOfFile(String path, String defaultVal) {
-        try (BufferedReader br = new BufferedReader(
+        try(BufferedReader br = new BufferedReader(
                 new FileReader(path))) {
             return br.readLine();
-        } catch (IOException e) {
+        } catch (IOException e){
             return defaultVal;
         }
+
     }
+
 
     public static void main(String[] args) throws IOException {
         String path = args[0];
